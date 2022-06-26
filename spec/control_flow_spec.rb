d7fb87ba9282c323308b8ea16ec7c1aa257b0573
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-26 15:43:13 -0600

    commit tree faa035ca59184ed0e3810ea1b80b73a425da0e36

--- spec/control_flow_spec.rb
+++ spec/control_flow_spec.rb
@@ -1,6 +1,49 @@
 require_relative 'spec_helper'
 require_relative '../control_flow'
 
+def admin_login(username, password)
+  if (username == 'admin' || username == 'ADMIN') && password == '12345'
+    'Access granted'
+  else
+    'Access denied'
+  end
+end
+
+def hows_the_weather(temp)
+  if temp < 40
+    "It's brisk out there!"
+  elsif temp >= 40 && temp <= 65
+    "It's a little chilly out there!"
+  elsif temp > 85
+    "It's too dang hot out there!"
+  else
+    "It's perfect out there!"
+  end
+end
+
+def fizzbuzz(num)
+  if num % 3 == 0 && num % 5 != 0
+    'Fizz'
+  elsif num % 3 != 0 && num % 5 == 0
+    'Buzz'
+  elsif num % 3 == 0 && num % 5 == 0
+    'FizzBuzz'
+  else
+    num
+  end
+end
+
+def calculator(operator, num1, num2)
+  case operator
+  when '+' then num1 + num2
+  when '-' then num1 - num2
+  when '*' then num1 * num2
+  when '/' then num1 / num2
+  else puts "Invalid operation!"
+  end
+end
+
+
 describe '#admin_login' do
   
   it 'returns "Access granted" if the username is "admin" and the password is "12345"' do
